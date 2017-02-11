515329c334dea0679627c26c505418e894ddfa74
Shane-S <shane.d.spoor@gmail.com>
2017-02-11 05:22:36 -0800

    Change for loop for compatibilty with phone's sh version, comment out incorrectly failing tests

--- src/test/external_tests_start.sh
+++ src/test/external_tests_start.sh
@@ -13,13 +13,15 @@ echo "Testing DROP on all TCP connections that send SYN-FIN, should be 100% loss
 hping3 $EXTERNAL_IP -S -F -c 5 -p 80
 
 #Testing UDP drop on 32768-32775
-for i in {32768..32775}; do
+for i in 32768 32769 32770 32771 32772 32773 32774 32775
+do
     echo "Testing drop incoming UDP between 32768-32775 ($i), should be 100% loss"
     hping3 $EXTERNAL_IP --udp -c 5 -p $i
 done
 
 #Testing TCP drop on 137-139
-for i in {137..139}; do
+for i in 137 138 139
+do
     echo "Testing drop incoming UDP between 137-139, should be 100% loss"
     hping3 $EXTERNAL_IP --udp -c 5 -p $i
 done
@@ -33,13 +35,15 @@ echo "Testing drop incoming TCP on port 111, should be 100% loss"
 hping3 $EXTERNAL_IP -S -c 5 -p 111
 
 #Testing TCP drop on 32768-32775
-for i in {32768..32775}; do
+for i in 32768 32769 32770 32771 32772 32773 32774 32775
+do
     echo "Testing drop incoming TCP between 32768-32775 ($i), should be 100% loss"
     hping3 $EXTERNAL_IP -S -c 5 -p $i
 done
 
 #Testing TCP drop on 137-139
-for i in {137..139}; do
+for i in 137 138 139
+do
     echo "Testing drop incoming TCP between 137-139 ($i), should be 100% loss"
     hping3 $EXTERNAL_IP -S -c 5 -p $i
 done
--- src/test/fw_post_test_start.sh
+++ src/test/fw_post_test_start.sh
@@ -29,30 +29,38 @@ getPacketCount()
 }
 
 # Check the drop-invalid chain's results before user-defined
-getRuleResults "$(iptables -vnL drop-invalid)"
-DROP_INVALID_RESULTS=("${RESULT[@]}")
-EXPECTED_INVALID_RESULTS=()
-EXPECTED_INVALID_RESULTS[0]=0   # -p tcp --tcp-flags ALL ACK,RST,SYN,FIN -j DROP
-EXPECTED_INVALID_RESULTS[1]=5   # -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
-EXPECTED_INVALID_RESULTS[2]=0   # -p tcp --tcp-flags SYN,FIN,PSH SYN,FIN,PSH -j DROP
-EXPECTED_INVALID_RESULTS[3]=0   # -p tcp --tcp-flags ALL ALL -j DROP
-EXPECTED_INVALID_RESULTS[4]=0   # -p tcp --tcp-flags ALL NONE -j DROP
-EXPECTED_INVALID_RESULTS[5]=5   # -p tcp --sport 23 -j DROP
-EXPECTED_INVALID_RESULTS[6]=5   # -p tcp --dport 23 -j DROP
-EXPECTED_INVALID_RESULTS[7]=40  # -i $EXTERNAL_DEVICE -p tcp -m multiport --dports 32768:32775 -j DROP
-EXPECTED_INVALID_RESULTS[8]=40  # -i $EXTERNAL_DEVICE -p udp -m multiport --dports 32768:32775 -j DROP
-EXPECTED_INVALID_RESULTS[9]=15  # -i $EXTERNAL_DEVICE -p tcp -m multiport --dports 137:139 -j DROP
-EXPECTED_INVALID_RESULTS[10]=15 # -i $EXTERNAL_DEVICE -p udp -m multiport --dports 137:139 -j DROP
-EXPECTED_INVALID_RESULTS[11]=10 # -i $EXTERNAL_DEVICE -p tcp -m multiport --dports 111,515 -j DROP
-
-for (( i=0; i<${#EXPECTED_INVALID_RESULTS[@]}; i++ )) do
-    echo "Checking packet counts for drop-invalid rule $i."
-    echo "                 pkts bytes target     prot opt in     out     source               destination"
-    echo "drop-invalid rule \$i:  \${DROP_INVALID_RESULTS[i]}"
-
-    getPacketCount "${DROP_INVALID_RESULTS[i]}"
-    doPacketCountTest drop-invalid $i ${EXPECTED_INVALID_RESULTS[i]} $COUNT
-done
+#
+# NOTE: All of these tests "fail" because none of these ports ever reach the FORWARD chain in the first place.
+# To cause this to pass, we'd first have to explicitly try to allow them (e.g. through user-defined allowed ports),
+# run the generated tests, and THEN check these results.
+#
+# We decided not to do that; instead, the user should look at the output from external_tests.sh to see that these
+# tests do actually work.
+#
+#getRuleResults "$(iptables -vnL drop-invalid)"
+#DROP_INVALID_RESULTS=("${RESULT[@]}")
+#EXPECTED_INVALID_RESULTS=()
+#EXPECTED_INVALID_RESULTS[0]=0   # -p tcp --tcp-flags ALL ACK,RST,SYN,FIN -j DROP
+#EXPECTED_INVALID_RESULTS[1]=5   # -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
+#EXPECTED_INVALID_RESULTS[2]=0   # -p tcp --tcp-flags SYN,FIN,PSH SYN,FIN,PSH -j DROP
+#EXPECTED_INVALID_RESULTS[3]=0   # -p tcp --tcp-flags ALL ALL -j DROP
+#EXPECTED_INVALID_RESULTS[4]=0   # -p tcp --tcp-flags ALL NONE -j DROP
+#EXPECTED_INVALID_RESULTS[5]=5   # -p tcp --sport 23 -j DROP
+#EXPECTED_INVALID_RESULTS[6]=5   # -p tcp --dport 23 -j DROP
+#EXPECTED_INVALID_RESULTS[7]=40  # -i $EXTERNAL_DEVICE -p tcp -m multiport --dports 32768:32775 -j DROP
+#EXPECTED_INVALID_RESULTS[8]=40  # -i $EXTERNAL_DEVICE -p udp -m multiport --dports 32768:32775 -j DROP
+#EXPECTED_INVALID_RESULTS[9]=15  # -i $EXTERNAL_DEVICE -p tcp -m multiport --dports 137:139 -j DROP
+#EXPECTED_INVALID_RESULTS[10]=15 # -i $EXTERNAL_DEVICE -p udp -m multiport --dports 137:139 -j DROP
+#EXPECTED_INVALID_RESULTS[11]=10 # -i $EXTERNAL_DEVICE -p tcp -m multiport --dports 111,515 -j DROP
+#
+#for (( i=0; i<${#EXPECTED_INVALID_RESULTS[@]}; i++ )) do
+#    echo "Checking packet counts for drop-invalid rule $i."
+#    echo "                 pkts bytes target     prot opt in     out     source               destination"
+#    echo "drop-invalid rule $i:  ${DROP_INVALID_RESULTS[i]}"
+#
+#    getPacketCount "${DROP_INVALID_RESULTS[i]}"
+#    doPacketCountTest drop-invalid $i ${EXPECTED_INVALID_RESULTS[i]} $COUNT
+#done
 
 getRuleResults "$(iptables -vnL tcp-in)"
 TCP_IN_RESULTS=("${RESULT[@]}")
